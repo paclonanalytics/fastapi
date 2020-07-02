378d909bb190b102779d556107e16c13b90c26aa
paclonanalytics <paclonanalytics@gmail.com>
2020-07-02 14:22:34 +0300

    ebuchka

--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -9,17 +9,17 @@ def read_root():
     return {"Hello": "World"}
 
 
-# def celery_on_message(body):
-#     print(body)
+def celery_on_message(body):
+    print(body)
 
-# def background_on_message(task):
-#     print(task.get(on_message=celery_on_message, propagate=False))
+def background_on_message(task):
+    print(task.get(on_message=celery_on_message, propagate=False))
 
 
-# @app.get("/{word}")
-# async def root(word: str, background_task: BackgroundTasks):
-#     task = celery_app.send_task(
-#         "worker.celery_worker.test_celery", args=[word])
-#     print(task)
-#     background_task.add_task(background_on_message, task)
-#     return {"message": "Word received"}
+@app.get("/{word}")
+async def root(word: str, background_task: BackgroundTasks):
+    task = celery_app.send_task(
+        "worker.celery_worker.test_celery", args=[word])
+    print(task)
+    background_task.add_task(background_on_message, task)
+    return {"message": "Word received"}
